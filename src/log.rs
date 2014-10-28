ef003f57731793e5d18d4c2968085c47ddda1337
Bjørnar Grip Fjær <bjornar.fjar@gmail.com>
2014-10-28 19:03:29 +0100

    Adding possibility of differentiating logs based on their logged distance.
    Since floats cannot implement complete equality, they cannot be used in
    a HashMap. Because all logs up until now has been whole integers, it's
    implemented by converting the distances to integers and using them as
    keys. This is obviously quite useless.

--- src/log.rs
+++ src/log.rs
@@ -2,6 +2,8 @@ extern crate sqlite3;
 
 use sqlite3::types::*;
 
+use std::collections::HashMap;
+
 pub struct Log {
     pub rssi: int,
     pub tx_power: int,
@@ -9,6 +11,7 @@ pub struct Log {
     pub mac_address: String,
 }
 
+/// Retrieves logs from and SQLite database.
 pub fn get_logs(db_file: &str) -> Option<Vec<Log>> {
     let mut logs = Vec::new();
     let db: sqlite3::Database = match sqlite3::open(db_file) {
@@ -18,7 +21,6 @@ pub fn get_logs(db_file: &str) -> Option<Vec<Log>> {
             return None;
         }
     };
-    println!("Opened database '{}'", db_file);
 
     let option = None;
 
@@ -49,4 +51,26 @@ pub fn get_logs(db_file: &str) -> Option<Vec<Log>> {
     }
 
     Some(logs)
+}
+
+impl Clone for Log {
+    fn clone(&self) -> Log {
+        Log{rssi: self.rssi, tx_power: self.tx_power, distance: self.distance, mac_address: self.mac_address.clone()}
+    }
+}
+
+pub fn split_based_on_distance(logs: &Vec<Log>) -> HashMap<i64, Vec<Log>> {
+    let mut distances: HashMap<i64, Vec<Log>> = HashMap::new();
+    let mut distance: i64;
+    for log in logs.iter() {
+        distance = log.distance as i64;
+        if distances.contains_key(&distance) {
+            distances.get_mut(&distance).push(log.clone());
+        } else {
+            let mut new_list = Vec::new();
+            new_list.push(log.clone());
+            distances.insert(distance, new_list);
+        }
+    }
+    distances
 }
